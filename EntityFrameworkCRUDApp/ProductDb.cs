673a9f039242025fcc79d0e7af078d53b6e2c431
Kramer Jensen <Kramerjensend@gmail.com>
2021-01-23 19:59:14 -0800

    Implemented the Add and Delete methods of the DB class

--- EntityFrameworkCRUDApp/ProductDb.cs
+++ EntityFrameworkCRUDApp/ProductDb.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +15,21 @@ namespace EntityFrameworkCRUDApp
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Adds a product to the database.
+        /// Returns the product with the <see cref="Product.ProductId"></see> column
+        /// (Identity column) populated
+        /// </summary>
+        /// <param name="p">The product to be added</param>
         public static Product Add(Product p)
         {
-            throw new NotImplementedException();
+            using (ProductContext context = new ProductContext())
+            {
+                context.Products.Add(p);  //  Prepares an INSERT query
+                context.SaveChanges();    //  Executes all pending INSERT/UPDATE/DELETE queries
+
+                return p;
+            }       
         }
 
         public static Product Update(Product p)
@@ -26,7 +39,18 @@ namespace EntityFrameworkCRUDApp
 
         public static void Delete(Product p)
         {
-            throw new NotImplementedException();
+            using (ProductContext context = new ProductContext())
+            {
+                //  Write EF output to the console
+                context.Database.Log = Console.WriteLine;
+
+                //  Telling EF (Entity Framework) that the product 
+                //  is already in the DB and should be deleted
+                context.Entry(p).State = EntityState.Deleted;
+
+                //  Executes DELETE query
+                context.SaveChanges();
+            }    
         }
 
         public static void Delete(int id)
